d7bc45d594cd86efa50906dfa058f7cf8affe04a
Matheo <matheo.martin@epitech.eu>
2023-06-29 12:10:25 +0200

    Add tests for coverage

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,9 +4,10 @@
 TEST_CASE("Factorials are computed", "[factorial]")
 {
     factorial factorial;
-  REQUIRE(factorial.factorial_function(0) == 1);
-  REQUIRE(factorial.factorial_function(1) == 1);
-  REQUIRE(factorial.factorial_function(2) == 2);
-  REQUIRE(factorial.factorial_function(3) == 6);
-  REQUIRE(factorial.factorial_function(10) == 3628800);
+    REQUIRE(factorial.factorial_function(-2) == -1);
+    REQUIRE(factorial.factorial_function(0) == 1);
+    REQUIRE(factorial.factorial_function(1) == 1);
+    REQUIRE(factorial.factorial_function(2) == 2);
+    REQUIRE(factorial.factorial_function(3) == 6);
+    REQUIRE(factorial.factorial_function(10) == 3628800);
 }
