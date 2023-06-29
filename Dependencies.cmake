269e72db1c09530439e25cbb760a63fcafa65850
Matheo <matheo.martin@epitech.eu>
2023-06-29 11:57:44 +0200

    Fix cmake compilation when catch2 isn't installed

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -3,14 +3,15 @@ include(cmake/CPM.cmake)
 # Done as a function so that updates to variables like
 # CMAKE_CXX_FLAGS don't propagate out to other
 # targets
+
+find_package(Catch2 QUIET)
+
 function(myproject_setup_dependencies)
 
   # For each dependency, see if it's
   # already been provided to us by a parent project
 
-  find_package(Catch2 QUIET)
-
-  if(NOT TARGET Catch2::Catch2WithMain AND CATCH2_FOUND EQUAL 0)
+  if(NOT TARGET Catch2::Catch2WithMain AND Catch2_FOUND EQUAL 0)
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
   endif()
 
